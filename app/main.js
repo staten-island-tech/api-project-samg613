8f84045ca0d84ec547ee787ecf817e39d0f97a0f
samg613 <samuelg380@nycstudents.net>
2024-11-19 14:00:36 -0500

    fgff

--- app/main.js
+++ app/main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+async function getData { 
+    try {  
+        const response = await fetch ("https://valorant-api.com/v1/agents");
+        if (response.status != 200) {
+        throw new Error(response); 
+        } else {
+            const data = await response.json();
+            console.log(data.data);
+            data.data.forEach((agent) => console.log(agent.displayName));
+            //document.querySelector("div").insertAdjacentHTML("afterbegin", `<h1>${agent.displayName}</h1›)
+        }
+    } catch (error) {
+        alert("hey i could not find that agent");
+    }
+    }
