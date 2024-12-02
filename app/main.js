9b4ed37a6df7a9cab13204361daec3e6e765602b
samg613 <samuelg380@nycstudents.net>
2024-12-02 14:02:22 -0500

    ah

--- app/main.js
+++ app/main.js
@@ -19,31 +19,32 @@ async function getData(query = "", language = "en") {
     } else {
       const data = await response.json();
 
-      if (data.articles.length === 0) {
-        newsContainer.innerHTML =
-          "<p>No news found for your search. Please try again.</p>";
-      } else {
-        newsContainer.innerHTML = "";
+      newsContainer.textContent = "";
 
-        data.articles.forEach((article) => {
-          const articleHTML = `
-            <div class="bg-gray-200 p-4 m-2 rounded shadow-md hover:shadow-lg transition duration-300">
-              <h2 class="text-xl font-semibold text-gray-800 hover:text-blue-600">${
-                article.title
-              }</h2>
-              <p class="text-sm text-gray-600">Published on: ${new Date(
-                article.publishedAt
-              ).toLocaleDateString()}</p>
-              <p class="text-sm text-gray-700 mt-2">${
-                article.description || "No description available."
-              }</p>
-              <a href="${
-                article.url
-              }" target="_blank" class="text-blue-500 hover:text-blue-700 mt-4 block transition duration-300">Read more</a>
-            </div>
+      if (data.articles.length === 0) {
+        newsContainer.insertAdjacentHTML(
+          "beforeend",
+          "<p> No news found for your search. Please try again.</p>"
+        );
+      } else {
+        for (let i = 0; i < data.articles.length; i++) {
+          const article = data.articles[i];
+          const articleHTML = ` 
+            <div class="bg-gray-200 p-4 m-2 rounded shadow-md hover:shadow-lg transition duration-300"> <h2 class="text-xl font-semibold text-gray-800 hover:text-blue-600">${
+              article.title
+            }</h2> <p class="text-sm text-gray-600">Published on: ${new Date(
+            article.publishedAt
+          ).toLocaleDateString()}</p> 
+            <p class="text-sm text-gray-700 mt-2">${
+              article.description || "No description available."
+            }</p> 
+            <a href="${
+              article.url
+            }" target="_blank" class="text-blue-500 hover:text-blue-700 mt-4 block transition duration-300">Read more</a> 
+            </div> 
           `;
           newsContainer.insertAdjacentHTML("beforeend", articleHTML);
-        });
+        }
       }
     }
   } catch (error) {
